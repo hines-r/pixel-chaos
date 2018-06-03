2a58fa63834bbe262bbf4f8f0efd69768c8731bb
hines-r <39021120+hines-r@users.noreply.github.com>
2018-06-03 15:07:54 -0600

    Added new unit named Tidalmancer

---
+++ Defense Game/Assets/Scripts/Spells/TidalWave.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class TidalWave : Attack
+{
+    [Header("Properties")]
+    public float speed = 5f;
+
+    private readonly float startPositionX = -4f;
+    private readonly float distanceXTillFade = 0f;
+
+    private Animator anim;
+    private BoxCollider2D bc2d;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        bc2d = GetComponent<BoxCollider2D>();
+
+        transform.position = new Vector3(startPositionX, Target.transform.position.y, Target.transform.position.z);
+    }
+
+    void Update()
+    {
+        transform.position += transform.right * speed * Time.deltaTime;
+
+        if (transform.position.x >= distanceXTillFade)
+        {
+            StartCoroutine(SelfDestruct());
+        }
+    }
+
+    IEnumerator SelfDestruct()
+    {
+        float timeTillDisable = 2f;
+        float timeTillDestroy = 3.5f;
+
+        anim.SetTrigger("Destroy");
+
+        yield return new WaitForSeconds(timeTillDisable);
+
+        bc2d.enabled = false;
+
+        yield return new WaitForSeconds(timeTillDestroy - timeTillDisable);
+
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+
+        enemy.TakeDamage(Damage);
+    }
+}
