8e1aee0708fa5cbc7f39e890ee9da7798a6d2e29
hines-r <39021120+hines-r@users.noreply.github.com>
2018-06-13 20:08:29 -0600

    Implemented a script that enforces a 16:9 aspect ratio

---
+++ Defense Game/Assets/CameraAspect.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraAspect : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Camera>().aspect = 16f / 9f;
+    }
+}
