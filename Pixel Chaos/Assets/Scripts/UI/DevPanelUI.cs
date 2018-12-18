a938239d9bed5c28418a98d6fdb63d6079d2fbd9
hines-r <39021120+hines-r@users.noreply.github.com>
2018-12-18 16:25:03 -0700

    The developer panel buttons and toggles now work properly

--- Pixel Chaos/Assets/Scripts/UI/DevPanelUI.cs
+++ Pixel Chaos/Assets/Scripts/UI/DevPanelUI.cs
@@ -1,18 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Animator))]
 public class DevPanelUI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Toggle godModeToggle;
+    public Toggle goldGemToggle;
+
+    private Animator anim;
+    private GameMaster gm;
+
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        gm = GameMaster.instance;
+
+        godModeToggle.isOn = gm.isImmortal;
+        goldGemToggle.isOn = gm.isBurdenedWithMoney;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowDevPanel()
     {
-        
+        gameObject.SetActive(true);
+    }
+
+    IEnumerator StartHideAnimation()
+    {
+        float delay = .1f;
+
+        anim.SetTrigger("Hide");
+        yield return new WaitForSeconds(delay);
+
+        gameObject.SetActive(false);
+    }
+
+    public void HideDevPanel()
+    {
+        StartCoroutine(StartHideAnimation());
+    }
+
+    public void ToggleGodMode()
+    {
+        gm.isImmortal = godModeToggle.isOn;
+    }
+
+    public void ToggleInfiniteMoney()
+    {
+        gm.isBurdenedWithMoney = goldGemToggle.isOn;
     }
 }
